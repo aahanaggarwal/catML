874dbe38350d6afb5b35310ef675b30d8907f262
Aashir Aumir <pulse.aashir@gmail.com>
2019-07-19 12:34:00 -0400

    add camera view and make app fullscreen

--- app/src/main/java/com/wastedsummer/catml/MainActivity.java
+++ app/src/main/java/com/wastedsummer/catml/MainActivity.java
@@ -1,14 +1,52 @@
 package com.wastedsummer.catml;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.camerakit.CameraKitView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CameraKitView mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mCamera = findViewById(R.id.camera);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mCamera.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCamera.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mCamera.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        mCamera.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mCamera.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
